c3a39a5d0ed249a01f94d63d28ae075558735cf9
Jaso Salt <Oatmas64134@gmail.com>
2016-08-02 14:50:05 -0400

    Added more files

--- src/textadv/textadv.java
+++ src/textadv/textadv.java
@@ -18,8 +18,8 @@ package textadv;/*
 import java.util.Scanner;
 
 
-class textadv {
-    private static final Scanner scanner = new Scanner(System.in);
+public class textadv {
+    static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String args[]) {
         //Declare Vars here.
@@ -41,7 +41,124 @@ class textadv {
                 System.out.println("Welcome to the Castle.");
                 System.out.println("You must make it to the end in order to escape.");
                 System.out.println("The more gold you find, the more points you get.");
+	            System.out.println("You are standing in a dungeon. There is a puddle of water.");
+	            System.out.println("If you wait, the jailer will come.");
+	            System.out.print("");
+	            System.out.print(">");
+	            String choice1 = scanner.next();
+	            switch (choice1) {
+		            case "wait":
+			            System.out.println("You wait a while. The Jailer comes.");
+			            System.out.println("You find a knife on the tray he brings.");
+			            System.out.print("");
+			            System.out.println(">");
+			            String choice2 = scanner.next();
+			            System.out.println(choice2);
+			            switch (choice2) {
+				            case "attack":
+					            System.out.println("You take the knife and stab him in the back.");
+					            System.out.println("You get 20 gold!");
+					            gold += 20;
+					            System.out.println("Exits are: north");
+					            System.out.println("Go where?");
+					            String choice3 = scanner.next();
+					            switch (choice3) {
 
+						            case "north":
+							            System.out.println("You're in a corridor, lined with cells.");
+							            System.out.println("There are stairs at either end.");
+							            System.out.println("Exits are: east, west");
+							            System.out.println("Go where?");
+							            String choice4 = scanner.next();
+							            switch (choice4){
+
+								            case "east":
+									            System.out.println("You're at the eastern end of the corridor");
+									            System.out.println("Exits are: up, west");
+									            System.out.println("Go Where?");
+									            String choice5 = scanner.next();
+									            switch (choice5){
+
+										            case "up":
+											            System.out.println("You're in a staircase.");
+											            System.out.println("Exits are: up, down");
+											            System.out.println("Go where?");
+											            String choice6 = scanner.next();
+											            switch (choice6) {
+												            case "up":
+													            System.out.println("You're in the royal bathroom.");
+													            System.out.println("You wonder why the dungeons are beneath the toilet.");
+													            System.out.println("There is a gilded door to the north.");
+													            System.out.println("Exits are: north");
+													            System.out.println("Go where?");
+													            String choice7 = scanner.next();
+													            switch (choice7) {
+														            case "north":
+															            System.out.println("You're in the king's bedchambers");
+															            System.out.println("There is a bouncy bed");
+															            System.out.println("There is a desk with a lot of drawers.");
+															            System.out.print("");
+															            System.out.println(">");
+															            String choice8 = scanner.next();
+															            switch (choice8) {
+																            case "bounce":
+																	            System.out.println("You bounced on the bed.");
+																	            System.out.println("The bed broke and you died.");
+																	            gameOver("Drowning");
+																            case "desk":
+																	            System.out.println("Do what with the desk?");
+																	            System.out.println(">");
+																	            String choice9 = scanner.next();
+																	            switch (choice9) {
+																		            case "inspect":
+																			            System.out.println("You look through all the drawers.");
+																			            System.out.println("You took a long time, and the king");
+																			            System.out.print(" arrived.");
+																			            System.out.println("He doesn't like you, and chops off");
+																			            System.out.println("your head.");
+																			            gameOver("Unfriendly king");
+																	            }
+															            }
+													            }
+											            }
+
+									            }
+
+							            }
+					            }
+
+
+			            }
+		            case "drink":
+			            System.out.println("Drink What?");
+			            System.out.print("");
+			            System.out.println(">Drink ");
+			            String choice3 = scanner.next();
+			            switch (choice3){
+				            case "puddle":
+					            System.out.println("You drank the puddle, and also rat pee.");
+
+					            gameOver("Contaminated Puddle");
+
+			            }
+		            case "force":
+			            System.out.println("Force What?");
+			            System.out.print("");
+			            System.out.println(">Force ");
+			            choice3 = scanner.next();
+			            switch (choice3) {
+				            case "door":
+					            System.out.println("You're too thirsty");
+			            }
+		            case "help":
+			            System.out.println("Commands are:");
+			            System.out.println("help-Display this message");
+			            System.out.println("gold-Displays your current gold.*");
+			            System.out.println("kills-Displays your kills*");
+			            System.out.println("info-Displays Information");
+			            System.out.println("quit-Quits the game.");
+			            System.out.println("A * denotes a WIP feature.");
+	            }
                 System.out.println("Thanks for Playing!");
 
             case "n":
